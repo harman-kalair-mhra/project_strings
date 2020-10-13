30bc2790426da7b96d2c26aa9d1b677380cbfcbe
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-10-13 11:33:54 +0100

    average word length added with readline

--- src/services/avgLength.js
+++ src/services/avgLength.js
@@ -1,14 +1,24 @@
-var sentence = function (str) {
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function Average (str) {
   var wordCount = str.split(" ").length;
   var wordArray = str.split(" ");
-  var wordAvg = 0;
-  for (var i = 0; i < wordCount.length; i++) {
-    wordAvg += wordArray[i].length;
+  var string = 0;
+  for (var i = 0; i < wordCount; i++){
+    string += wordArray[i].length;
   }
-  var avgLen = wordAvg / wordCount;
-  console.log("Word length average: " + avgLen);
+  var avgLen = string / wordCount;
+  return avgLen
 };
 
-sentence(
-  "This function will analyze strings djskdj wjdsksjks jsdkjk djsksdjks jsdksjdks sjdksjdsk"
-);
+rl.question("Type in words for Average word Length: ", (answer) => {
+  console.log("Your Average count is " + Average(answer))
+  rl.close()
+})
+
+// console.log(Average("This function will analyze strings"));
