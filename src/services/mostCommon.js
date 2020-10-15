6ed233b8bf49d5dec9701f3c5a076dd5d1c57657
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-10-15 11:59:49 +0100

    Most common completed

--- src/services/mostCommon.js
+++ src/services/mostCommon.js
@@ -1,21 +1,14 @@
-var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]; //array
-var mf = 1; //default maximum frequency
-var m = 0;  //counter
-var item;  //to store item with maximum frequency
-for (var i=0; i<arr1.length; i++)    //select element (current element)
-{
-        for (var j=i; j<arr1.length; j++)   //loop through next elements in array to compare calculate frequency of current element
-        {
-                if (arr1[i] == arr1[j])    //see if element occurs again in the array
-                 m++;   //increment counter if it does
-                if (mf<m)   //compare current items frequency with maximum frequency
-                {
-                  mf=m;      //if m>mf store m in mf for upcoming elements
-                  item = arr1[i];   // store the current element.
-                }
-        }
-        m=0;   // make counter 0 for next element.
-}
+function mostCommon(str) {
+        let words = str.replace(/[.]/g, '').split(/\s/);
+        let freqMap = {};
+        words.forEach(function(w) {
+            if (!freqMap[w]) {
+                freqMap[w] = 0;
+            }
+            freqMap[w] += 1;
+        });
+    
+        return freqMap;
+    }
 
-console.log(item + " ( " + mf + " times ) ");
-
+      module.exports = mostCommon
