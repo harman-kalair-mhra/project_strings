2ee4321f50c2cbafef3d4f34bd3fef069aef929b
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-10-22 13:07:34 +0100

    exported main function

--- src_ts/main.ts
+++ src_ts/main.ts
@@ -5,19 +5,27 @@ import MostCommon from "./services/mostCommon"
 
 let readline = require('readline')
 
-function main():void {
+export default function main(str: string) :string {
 
-let rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+  return (
+    `Character Count: ${(CharacterCount(str))}
+    Average Count: ${Average(str)}
+    Word Count: ${WordCount(str)}
+    Most Common: ${JSON.stringify(MostCommon(str))} 
+    `
+  )
 
-rl.question("Type in words for analysis: ", (answer: string) : void => {
-    console.log("Your Character count is:", CharacterCount(answer)), 
-    console.log("Your Average word Length is:", Average(answer)), 
-    console.log("Your Word count is:", WordCount(answer))
-    console.log("Your Most Common word is:", MostCommon(answer))
-    rl.close()
-  })
+// let rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
+
+// rl.question("Type in words for analysis: ", (answer: string) : void => {
+//     console.log("Your Character count is:", CharacterCount(answer)), 
+//     console.log("Your Average word Length is:", Average(answer)), 
+//     console.log("Your Word count is:", WordCount(answer))
+//     console.log("Your Most Common word is:", MostCommon(answer))
+//     rl.close()
+//   })
 }
-main()
+// main()
