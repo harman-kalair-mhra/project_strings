67d072f6733ba992770f50ac946e03a35dfb74d2
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-11-12 17:39:53 +0000

    installed styled components and created a text box

---
+++ frontend/src/components/Textbox.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import styled from "styled-components"
+
+const StyledTextBox = styled.textarea`
+width: 100%;
+height: 150px;
+padding: 12px 20px;
+box-sizing: border-box;
+border: 2px solid #ccc;
+border-radius: 4px;
+background-color: #f8f8f8;
+font-size: 16px;
+resize: none;
+`;
+
+export default StyledTextBox
